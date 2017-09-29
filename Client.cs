9aa1d24e4bdf9fdaf2621dd1640d943dba5c2651
Jen <jnnfrlocke@gmail.com>
2017-09-29 10:04:35 -0500

    Move get user to server side client

--- Client.cs
+++ Client.cs
@@ -16,26 +16,9 @@ namespace ChatRoom
         NetworkStream stream;
         int userID;
         //login
-        public void GetUser()
-        {
-            Console.WriteLine("Are you a new user?");
-            string newUser = UI.GetInput().ToLower();
-            if (newUser == "yes" || newUser == "y")
-            {
-                Console.WriteLine("Please type a user name.");
-                byte[] userName = Encoding.ASCII.GetBytes(UI.GetInput()); // TODO: then assign user id on server
-                stream.Write(userName, 0, userName.Count()); //send/save to dictionary on server
-                // TODO: get userID from server
-                Console.WriteLine($"Your user ID is {userID}. Keep this handy to login again. \nPress enter to continue.");
-                Console.ReadLine();
-            }
-            else if (newUser == "no" || newUser == "n")
-            {
-                Console.WriteLine("Please enter your user ID.");
-
-            }
+        
                         
-        }
+        
         public void Send()
         {
             string messageString = UI.GetInput();
